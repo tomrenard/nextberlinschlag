de4b514623da3e5e5cf409a7d603152f23b42a50
Tom Renard <tom.renard@zoho.com>
2022-05-18 19:32:17 +0200

    feat: adjusted header

--- front/components/Header.tsx
+++ front/components/Header.tsx
@@ -1,16 +1,7 @@
 function Header() {
   return (
-    <header className="bg-[url('../assets/images/bg-test.jpg')]">
-      <div className="flex flex-col justify-center max-w-sm">
-        <h1 className="text-white uppercase">Berlin Schlag</h1>
-        <p className="text-white uppercase">
-          Berlin Schlag has been created to talk to you directly. These days,
-          it's rare to feel familiar with a media's editorial policy. That's why
-          BERLINSchlag is spreading stories as a friend could do during a party
-          or a sunday walk. So please open your ears, your eyes and take your
-          time to read what we are sharing with you!
-        </p>
-      </div>
+    <header >
+        <p>header</p>
     </header>
   );
 }
